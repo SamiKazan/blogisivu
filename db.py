3f76a36cd6c39295915a2740d29f89e1679f9311
SamiKazan <sami.kazan@helsinki.fi>
2025-03-21 17:33:36 +0200

    deployement additions

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 import psycopg2
+from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 db = SQLAlchemy(app)
